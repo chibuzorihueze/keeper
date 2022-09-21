14608c36e44061fef170d99369e36635aa448ef8
Chibuzor Ihueze <krisykord@gmail.com>
2022-09-21 03:21:48 +0100

    adding components in a folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./header";
-import Footer from "./footer";
-import Note from "./note";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Note from "./components/note";
 
 
 function App() {
